7916dabca4d7be6136d2521b5427b8d7a01f7c8b
King(qpwakaba) <King.qpwakaba@gmail.com>
2018-06-10 00:50:16 +0900

    Change project name JsonParser to qpJsonParser

--- JsonParser/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("JsonParserTest (.NET Framework)")]
--- qpJsonParserTest (.NET Framework)/Properties/AssemblyInfo.cs
+++ qpJsonParserTest (.NET Framework)/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("JsonParserTest (.NET Framework)")]
+[assembly: AssemblyTitle("qpJsonParserTest (.NET Framework)")]
 [assembly: AssemblyDescription("Test code of JsonParser")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("qpwakaba")]
-[assembly: AssemblyProduct("JsonParserTest")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("qpJsonParser")]
+[assembly: AssemblyCopyright("NYSL Version 0.9982")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
