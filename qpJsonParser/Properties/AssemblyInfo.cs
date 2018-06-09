323d62e13d2967c8a29fca60608d180b056161bf
King(qpwakaba) <King.qpwakaba@gmail.com>
2018-06-10 01:40:04 +0900

    Change target framework of qpJsonParser to .NET Framework

--- qpJsonParser/Properties/AssemblyInfo.cs
+++ qpJsonParser/Properties/AssemblyInfo.cs
@@ -1,3 +1,35 @@
-﻿using System.Runtime.CompilerServices;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-[assembly: InternalsVisibleTo("qpJsonParserTest (.NET Framework)")]
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("qpJsonParser")]
+[assembly: AssemblyDescription("a JSON parser")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("qpwakaba")]
+[assembly: AssemblyProduct("qpJsonParser")]
+[assembly: AssemblyCopyright("NYSL Version 0.9982")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、 
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("7966b319-1050-4594-964f-d1f20c7be297")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("qpJsonParser.Test")]
